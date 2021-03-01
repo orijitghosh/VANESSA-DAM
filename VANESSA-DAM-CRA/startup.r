9a1d2a5df75809436689cec927a6daa277524b9f
Arijit Ghosh <45988880+orijitghosh@users.noreply.github.com>
2021-03-01 09:01:42 +0530

    Added the "Cairo" graphics engine for reproducible script figures

--- VANESSA-DAM-CRA/startup.r
+++ VANESSA-DAM-CRA/startup.r
@@ -23,8 +23,9 @@ if (!require("shinyhelper")) install.packages("shinyhelper", dependencies = TRUE
 if (!require("shinyFiles")) install.packages("shinyFiles", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 if (!require("fs")) install.packages("fs", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 if (!require("data.table")) install.packages("data.table", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
+if (!require("Cairo")) install.packages("Cairo", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 
-if (is.installed("shiny") & is.installed("WaveletComp") & is.installed("ggetho") & is.installed("ggplot2") & is.installed("zeitgebr") & is.installed("readr") & is.installed("damr")
+if (is.installed("shiny") & is.installed("WaveletComp") & is.installed("ggetho") & is.installed("ggplot2") & is.installed("zeitgebr") & is.installed("readr") & is.installed("damr") & is.installed("Cairo")
 & is.installed("sleepr") & is.installed("behavr") & is.installed("dplyr") & is.installed("shinyalert") & is.installed("colourpicker") & is.installed("beepr") & is.installed("showtext") & is.installed("data.table")
 & is.installed("shinydashboard") & is.installed("shinycustomloader") & is.installed("shinythemes") & is.installed("shinyWidgets") & is.installed("shinyhelper") & is.installed("shinyFiles") & is.installed("fs")) {
   print(paste("Initialisation complete! VANESSA-DAM-CRA is now ready for use"), quote = F)
