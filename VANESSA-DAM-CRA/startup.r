d14f76c57d894118a4c1efe939ae57ff9e67f0f1
Arijit Ghosh <45988880+orijitghosh@users.noreply.github.com>
2021-07-27 16:00:48 +0530

    Added installation chain for ggridges

--- VANESSA-DAM-CRA/startup.r
+++ VANESSA-DAM-CRA/startup.r
@@ -28,11 +28,12 @@ if (!require("pracma")) install.packages("pracma", dependencies = TRUE, INSTALL_
 if (!require("lubridate")) install.packages("lubridate", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 if (!require("signal")) install.packages("signal", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 if (!require("tidyr")) install.packages("tidyr", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
+if (!require("ggridges")) install.packages("ggridges", dependencies = TRUE, INSTALL_opts = "--no-multiarch")
 
 if (is.installed("shiny") & is.installed("WaveletComp") & is.installed("ggetho") & is.installed("ggplot2") & is.installed("zeitgebr") & is.installed("readr") & is.installed("damr") & is.installed("Cairo") & is.installed("tidyr")
 & is.installed("sleepr") & is.installed("behavr") & is.installed("dplyr") & is.installed("shinyalert") & is.installed("colourpicker") & is.installed("beepr") & is.installed("showtext") & is.installed("data.table")
 & is.installed("shinydashboard") & is.installed("shinycustomloader") & is.installed("shinythemes") & is.installed("shinyWidgets") & is.installed("shinyhelper") & is.installed("shinyFiles") & is.installed("fs")
-& is.installed("signal") & is.installed("lubridate") & is.installed("pracma")) {
+& is.installed("signal") & is.installed("lubridate") & is.installed("pracma") & is.installed("ggridges")) {
   print(paste("Initialisation complete! VANESSA-DAM-CRA is now ready for use"), quote = F)
 } else {
   print(paste("Error: One or more packages may not have installed correctly"), quote = F)
